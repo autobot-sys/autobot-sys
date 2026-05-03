df8c05023316e8f9290f33e5e10fd58b6bda8177
autobot-sys <autobot.concept@gmail.com>
2026-05-03 19:12:10 +0000

    Update footer message in zivudp.sh

--- panel/zivudp.sh
+++ panel/zivudp.sh
@@ -140,7 +140,7 @@ draw_header() {
   echo "  ║   ╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝           ║"
   echo -e "  ║${NC}                                                          ${C}║${NC}"
   echo -e "  ${C}║${NC}  ${Y}${BOLD}  ━━━━━━ Z I V P N  U D P  P A N E L ━━━━━━   ${NC}  ${C}        ║${NC}"
-  echo -e "  ${C}║${NC}  ${DIM}        github.com/autobot-sys  -  v${PANEL_VERSION}${NC}  ${C}             ║${NC}"
+  echo -e "  ${C}║${NC}  ${DIM}        telegram: t.me/noobsvpn  -  v${PANEL_VERSION}${NC}  ${C}             ║${NC}"
   echo -e "${C}  ╚══════════════════════════════════════════════════════════╝${NC}"
 }
 
